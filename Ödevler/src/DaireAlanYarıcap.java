41d10d7277d7c1fe9ce814d32c3892755647d76b
OsmanTekeli <Tekelii.Osman@gmail.com>
2025-03-12 01:26:14 +0300

    Create DaireAlanYarıcap.java

---
+++ Ödevler/src/DaireAlanYarıcap.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.util.Scanner;
+
+public class DaireAlanYarıcap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int r;
+        double pi = 3.14;
+
+        System.out.print("Dairenin Yarıçapını Griniz: ");
+
+        r = input.nextInt();
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre);
+    }
+}
